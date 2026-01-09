1e09e88870b6061d7b7b8112e94f34e3533b3a0d
dsilvnas <daniel.silvanascimento@nttdata.com>
2026-01-09 14:38:17 -0300

    fix(deploy): correct base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/simulado-ctfl-4.0-profissional/',
+      base: '/ctfl4.0/',
       server: {
         port: 3000,
         host: '0.0.0.0',
